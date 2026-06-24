05105ae62f878e5f463ac8f70b9f931c24598780
Gaurav Kansara <www.gauravkansara28@gmail.com>
2026-06-24 14:58:25 +0530

    Bake AWS Amplify environment variables into Next.js server runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_AWS_COGNITO_REGION: process.env.NEXT_PUBLIC_AWS_COGNITO_REGION,
+    NEXT_PUBLIC_AWS_USER_POOL_ID: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID,
+    NEXT_PUBLIC_AWS_USER_POOL_CLIENT_ID: process.env.NEXT_PUBLIC_AWS_USER_POOL_CLIENT_ID,
+  }
 };
 
 export default nextConfig;
